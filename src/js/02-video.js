64ac7b3dd190577d398a19d1d43effe83bd4026b
tSolodkaya <solodkayagit@gmail.com>
2023-02-20 17:03:14 +0100

    task-2 video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import { throttle } from 'throttle-debounce';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(1000, onCurrentTime));
+function onCurrentTime(data) {
+  console.log(data.seconds);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+if (currentTime) {
+  player.setCurrentTime(currentTime).catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        alert(error.name);
+        break;
+
+      default:
+        alert(error.name);
+        break;
+    }
+  });
+}
