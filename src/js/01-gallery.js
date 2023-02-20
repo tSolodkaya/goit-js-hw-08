8ad4c0da0e29ad07fea101085522c336fa1b4f12
tSolodkaya <solodkayagit@gmail.com>
2023-02-20 16:00:06 +0100

    task1 simpleLightBox library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galeryContainer = document.querySelector('.gallery');
+
+const renderGaleryItems = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+`;
+  })
+  .join('');
+
+galeryContainer.innerHTML = renderGaleryItems;
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  showCounter: false,
+  docClose: true,
+  sourceAttr: 'href',
+});
